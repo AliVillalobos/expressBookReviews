e2f74d6a52b3047e8d5f31a82bf157b34e29f3d2
AliVillalobos <ali.villalobos.dev@gmail.com>
2024-12-20 13:44:21 -0500

    Project done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,7 @@ const { JsonWebTokenError } = require('jsonwebtoken');
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios').default;
 
 
 public_users.post("/register", (req,res) => {
@@ -25,42 +26,84 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-
-    res.status(201).send(JSON.stringify(books));
+public_users.get('/', async function (req, res) {
+    try{
+        const booksData = await new Promise((resolve,reject)=>{
+            setTimeout(()=>{
+            resolve(books);
+        }, 10000);
+    });
+    res.status(200).json(booksData);
+    }catch(error){
+        res.status(500).send('Error to obtain the data: ' + error.massage);
+    }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(JSON.stringify(books[isbn]));
+    const bookRequiere = books[req.params.isbn];
+
+    new Promise((resolve,reject)=> {
+        try{
+            resolve(bookRequiere);
+        }catch(error) {
+            res.status(500).send("Erro to obtain data: " + error.message);
+        }
+    })
+    .then((data)=>{
+        res.status(200).json(data);
+    })
+    .catch((error)=>{
+        res.status(500).send(error.massage);
+    });
+            
+   
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
+    try{
     const author = req.params.author;
-    let book = Object.values(books).filter(book => book.author === author);
-
+    const book = await new Promise((resolve,reject)=> {
+        const foundbook = Object.values(books).filter(book => book.author === author);
+        resolve(foundbook);
+    });
+    
     if(book.length === 0){
         return res.status(404).send({message: "No books found fot this author. "});
     }
     res.send(JSON.stringify(book));
-
+    }catch(error){
+        res.status(500).send("Erro occurs to process the request: " + error.message);
+    }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
-    let bKey = null;
-    for(let key in books){
-        if(books.hasOwnProperty(key)){
-            let book = books[key];
-            if(book.title === title){
-                    bKey = key;
+    new Promise((resolve,reject)=>{
+        let bKey = null;
+        for(let key in books){
+            if(books.hasOwnProperty(key)){
+                let book = books[key];
+                if(book.title === title){
+                        bKey = key;
+                        break;
+                }
             }
         }
-    }
-      res.send(JSON.stringify(books[bKey]));
+        if(bKey){
+            resolve(books[bKey]);
+        }else{
+            reject("No book found with given title");
+        }
+    })
+    .then((book) => {
+    res.status(200).json(book)
+    })
+    .catch((error)=> {
+        res.status(404).send({message: error});
+    });
 });
 
 
